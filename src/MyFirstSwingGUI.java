86ce95f9a032a73cc2b7a78ad71c777716d3315c
paradisefall123 <league@Mac-mini-4.local>
2018-07-28 11:29:20 -0700

    I finished my first swing GUI and photo quiz

--- src/MyFirstSwingGUI.java
+++ src/MyFirstSwingGUI.java
@@ -15,31 +15,32 @@ import javax.swing.JPanel;
 public class MyFirstSwingGUI {
 	public static void main(String[] args) {
 		//1. Create and initialize an object of the JFrame class
-
+             JFrame wendy= new JFrame() ;
 		//2. Set your JFrame object to be visible
-
+             wendy.setVisible(true);
 		//3. Run your program. Do you see your window? It's probably very small.
 		
 		//4. Set the default close operation to JFrame.EXIT_ON_CLOSE
-
+             	wendy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		//5. Create and initialize an object of the JPanel class
-
+             	JPanel guadalupe=new JPanel();
 		//6. Create and initialize an object of the JLabel class
-
+                 JLabel gonzalez= new JLabel();
 		//7. Set the text of the JLabel to a lovely greeting.
-
+                    gonzalez.setText("Hello");
 		//8. Add the JPanel object to the JFrame
-
+                    wendy.add(guadalupe);
 		//9. Add the JLabel object to the JPanel
-
+                    guadalupe.add(gonzalez);
 		//10. Pack your JFrame.
-
+                    	wendy.pack();
 		//11. Run your program again. Do you see your message.
 		
 		//12. Use the loadImage method to set the icon of the JLabel object.  
-
+                  ImageIcon Valentin=loadImage();
+                  gonzalez.setIcon(Valentin);
 		//13. Re-pack the JFrame object.
-		
+                  	wendy.pack();
 		//14. Run the program one more time. Do you see the image?
 	}
 	
--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,27 +22,41 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+                String url="https://d2c8orla013wc0.cloudfront.net/from-blog/port_of_busan_south_korea.jpg";
 		// 2. create a variable of type "Component" that will hold your image
-
+                	Component image;
 		// 3. use the "createImage()" method below to initialize your Component
-
+                image=	createImage(url);
 		// 4. add the image to the quiz window
-
+                	quizWindow.add(image);
 		// 5. call the pack() method on the quiz window
-
+                	quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+         String answer=JOptionPane.showInputDialog("What country was the picture taken in?");
 		// 7. print "CORRECT" if the user gave the right answer
-
+             	if(answer.equalsIgnoreCase("South korea")) {
+             		JOptionPane.showMessageDialog(null, "CORRECT");
+             	}
+             	else {
+             		JOptionPane.showMessageDialog(null, "INCORRECT");
+             	}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+             		quizWindow.remove(image);
 		// 10. find another image and create it (might take more than one line of code)
-
+             		String memorial="http://www.dronestagr.am/wp-content/uploads/2018/01/Photo-Aug-23-7-42-24-PM-1200x800.jpg";
+             		Component pic;
+             		pic=createImage(memorial);
+             		quizWindow.add(pic);
+             		quizWindow.pack();
 		// 11. add the second image to the quiz window
-
+             			String quiz=JOptionPane.showInputDialog("What memorial is this?");
+             			if(quiz.equalsIgnoreCase("9/11 memorial")) {
+             				JOptionPane.showMessageDialog(null, "CORRECT");
+             			}else {
+             				JOptionPane.showMessageDialog(null, "INCORRECT");
+             			}
 		// 12. pack the quiz window
 
 		// 13. ask another question
