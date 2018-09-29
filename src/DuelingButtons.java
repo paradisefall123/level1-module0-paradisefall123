0d747e055d2b2837d29f12a288ece6402eba02d4
tactreconx <league@WTS-MM-11.local>
2018-09-29 11:28:23 -0700

    I finished Dueling Buttons and I need to put a pic of a fortune cookie. I need animal crackers

--- src/DuelingButtons.java
+++ src/DuelingButtons.java
@@ -28,25 +28,27 @@ public class DuelingButtons implements ActionListener {
 
 	private void createUI() {
 		// 1. Add the panel to the frame
-				panel.add(frame);
+			frame.add(panel);
 		// 2. Make the frame visible
-
+			frame.setVisible(true);
 		// 3. Set the text of the leftButton to "Click me!"
-
+			leftButton.setText("Click me!");
 		// 4. Set the text of the rightButton to "Click me!"
-
+			rightButton.setText("Click me!");
+			leftButton.setName("leftbutton");
+			rightButton.setName("rightbutton");
 		// 5. Add an action listener to the leftButton
-
+			leftButton.addActionListener(this);
 		// 6. Add an action listener to the rightButton
-
+			rightButton.addActionListener(this);
 		// 7. Add the leftButton to the panel
-
+			panel.add(leftButton);
 		// 8. Add the rightButton to the panel
-
+			panel.add(rightButton);
 		// 9. Pack the frame
-
+			frame.pack();
 		// 10. Set the title of the frame to "Demanding Buttons"
-
+			frame.setTitle("Demanding Buttons");
 	}
 
 	@Override
@@ -54,8 +56,21 @@ public class DuelingButtons implements ActionListener {
 		JButton buttonPressed = (JButton) arg0.getSource();
 		
 		
+		
 		/* If the buttonPressed was the leftButton....*/
-			// Set the text of the rightButton to "No, click Me!"
+			//Set the text of the rightButton to "No, click Me!"
+		
+			if(buttonPressed.getName().equals("leftbutton")) {
+				rightButton.setText("No, click Me!");
+				rightButton.setPreferredSize(BIG);
+				leftButton.setText("Click Me!");
+				leftButton.setPreferredSize(SMALL);
+			}else {
+				leftButton.setText("No, click Me!");
+				leftButton.setPreferredSize(BIG);
+				rightButton.setText("Click Me!");
+				rightButton.setPreferredSize(SMALL);
+			}
 			// Set the PREFERRED size of the rightButton to BIG
 			// Set the text of the leftButton to "Click Me!"
 			// Set the PREFERRED size of the leftButton to SMALL
---
+++ src/extra/FortuneCookie.java
@@ -0,0 +1,47 @@
+package extra;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+public class FortuneCookie implements ActionListener {
+	public static void main(String[] args) {
+		FortuneCookie fc = new FortuneCookie();
+		fc.showButton();
+
+	}
+
+	public void showButton() {
+
+		JFrame frame = new JFrame();
+		JButton button = new JButton();
+		System.out.println("Button clicked");
+		frame.setVisible(true);
+		frame.add(button);
+		button.addActionListener(this);
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+		int rand = new Random().nextInt(5);
+		if(rand==0) {
+			JOptionPane.showMessageDialog(null, "You will live long enough to open many fortune cookies.");
+		}else if(rand==1) {
+			JOptionPane.showMessageDialog(null, "The fortune you seek is in another cookie.");
+		}else if(rand==2) {
+			JOptionPane.showMessageDialog(null, "Don’t worry about money. The best things in life are free.");
+		}else if(rand==3) {
+			JOptionPane.showMessageDialog(null, "Don’t pursue happiness – create it.");
+		}else if(rand==4) {
+			JOptionPane.showMessageDialog(null, "If you want the rainbow, you have to tolerate the rain.");
+		}
+	}
+
+}
