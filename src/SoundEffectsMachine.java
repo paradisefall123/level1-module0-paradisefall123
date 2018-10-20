601860f1e84fa1af306caae691467435ca8ff8bc
League <league@Mac-mini.local>
2018-10-20 11:28:07 -0700

    i need to do sound effects machine

--- src/SoundEffectsMachine.java
+++ src/SoundEffectsMachine.java
@@ -1,9 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class SoundEffectsMachine {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class SoundEffectsMachine implements ActionListener {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		SoundEffectsMachine fc= new SoundEffectsMachine();
+	}public void showButton() {
+	     System.out.println("Button clicked");
+	     JFrame frame=new JFrame();
+	     JButton button=new JButton();
+	     frame.setVisible(true);
+	     frame.add(button);
+	     button.addActionListener(this);
 	}
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
