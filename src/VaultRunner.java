437faf690a82458aab1fd8c716b9ace55e4af4c5
paradisefall123 <league@WorthyMini-2.local>
2019-01-05 11:28:12 -0800

    do vault

--- src/VaultRunner.java
+++ src/VaultRunner.java
@@ -2,6 +2,6 @@
 public class VaultRunner {
 	public static void main(String[] args) {
 	Vault vovo	=new Vault(97);
-System.out.println(vovo.tryCode(97));	
+System.out.println(vovo.tryCode(1));	
 	}
 }
